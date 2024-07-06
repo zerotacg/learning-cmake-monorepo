e6d319e1862f6f9b8f3226b2fc6c587aa15d3b8e
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-07-06 06:54:40 +0000

    add dependency of private lib to exported target

--- project-a/lib-foo/project-a/foo/foo.cpp
+++ project-a/lib-foo/project-a/foo/foo.cpp
@@ -1,11 +1,11 @@
 #include <project-a/foo/foo.hpp>
 #include <project-a/foo/other.hpp>
-#include <project-a/shared/shared.hpp>
+#include <project-a/special/special.hpp>
 
 #include <iostream>
 
 using namespace project_a::foo;
-using namespace project_a::shared;
+using namespace project_a::special;
 
 void project_a::foo::foo() {
     std::cout << "Hello foo! " << PRIVATE_VALUE << std::endl;
